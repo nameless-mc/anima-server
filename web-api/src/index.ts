3fec60b59b9ce8511d32db14bf5ed1845e088cdc
Nameless <tanisyun0331@icloud.com>
2022-07-17 13:54:57 +0900

    認証周りのapi調整

--- web-api/src/index.ts
+++ web-api/src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import errorHandler from "./error";
+import errorHandler, {
+  forbiddenException,
+  internalServerErrorException,
+} from "./error";
 import config from "./config";
 import session from "express-session";
 import { twitterOAuth2 } from "twitter-oauth2";
 import authenticationResource from "./resource/authentication_resource";
 import { connectDB as connectMySQL } from "./mysql";
+import { request } from "undici";
+import { createUser, getUser, User } from "./service/user_service";
 const app: express.Express = express();
 
 connectMySQL();
@@ -49,16 +54,86 @@ app.use(
   twitterOAuth2({
     client_id: process.env.TWITTER_CLIENT_ID,
     client_secret: process.env.TWITTER_CLIENT_SECRET,
-    redirect_uri: "http://localhost:3000/auth/twitter/callback",
+    redirect_uri: "http://localhost:3000/auth/twitter",
     scope: "tweet.read tweet.write users.read offline.access",
   })
 );
 
-app.use("/auth", authenticationResource);
+app.post("/api/signon", async (req: express.Request, res: express.Response) => {
+  const pref = req.body.pref;
+  const tokenSet = req.session.tokenSet;
+  if (!tokenSet || !tokenSet.access_token || !tokenSet.refresh_token) {
+    throw internalServerErrorException();
+  }
+  const { body } = await request("https://api.twitter.com/2/users/me", {
+    headers: {
+      Authorization: `Bearer ${tokenSet.access_token}`,
+    },
+  });
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("hogehoge")
+  const username = (await body.json()).data.name;
+  const twitter_id = (await body.json()).data.id;
+  const user = await createUser(
+    username,
+    tokenSet.access_token,
+    tokenSet.refresh_token,
+    pref,
+    twitter_id
+  );
+  console.log("create user: ", user);
+
+  res.send();
 });
+
+app.get("/", async (req: express.Request, res: express.Response) => {
+  const tokenSet = req.session.tokenSet;
+  if (!tokenSet || !tokenSet.access_token || !tokenSet.refresh_token) {
+    throw forbiddenException();
+  }
+  const { body } = await request("https://api.twitter.com/2/users/me", {
+    headers: {
+      Authorization: `Bearer ${tokenSet.access_token}`,
+    },
+  });
+
+  const twitter_id = (await body.json()).data.id;
+  const user = await getUser(twitter_id);
+  if (user) {
+    res.send("hello " + user.name);
+  } else {
+    res.send("there is not user data");
+  }
+});
+
+app.use(
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    const tokenSet = req.session.tokenSet;
+    if (!tokenSet || !tokenSet.access_token || !tokenSet.refresh_token) {
+      throw forbiddenException();
+    }
+    const { body } = await request("https://api.twitter.com/2/users/me", {
+      headers: {
+        Authorization: `Bearer ${tokenSet.access_token}`,
+      },
+    });
+    console.log("received tokens %j", req.session.tokenSet);
+
+    const data = (await body.json()).data;
+    const twitter_id = data.id;
+    const user = await getUser(twitter_id);
+    if (!user) {
+      res.redirect("/");
+    } else {
+      next();
+    }
+  }
+);
+
+app.use("/auth", authenticationResource);
 
 app.use(errorHandler);
 
